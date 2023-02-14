9e9e2a311095ebcf6381e6855f18e9594cd4ea3f
GFFritz <gabrielf.fritz@gmail.com>
2023-02-14 01:18:44 -0300

    Changes in folder structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 // Read Json File to translate - TO DO
-fetch('../resource/pt_br.json')
+fetch('../lang/pt_br.json')
     .then((response) => response.json())
     .then((json) => console.log(json.test));
