b17f6a7ec12224cd1e3a8fc89f584d51890173d1
GFFritz <gabrielf.fritz@gmail.com>
2023-06-05 17:20:43 -0300

    Alteracao de fonte, leitura do arquivo de traducoes e novo bloco mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 // Read Json File to translate - TO DO
 fetch('../lang/pt_br.json')
     .then((response) => response.json())
-    .then((json) => console.log(json.test));
+    .then((json) => console.log(json));
