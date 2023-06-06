c50e027a1c4aeb06d488360118ab6f9580be9c30
GFFritz <gabrielf.fritz@gmail.com>
2023-06-06 03:19:54 -0300

    Applied translate system based on user lang setting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,36 @@
+// Read user system language
+function getLangFile() {
+    let language = window.navigator.language;
+    let languageFistTwo = language.substr(0, 2); // To only keep the first 2 characters.
+
+    // Set the correct JSON file
+    if (languageFistTwo == 'pt') {
+        var langFile = "../lang/pt_br.json"
+    } else {
+        var langFile = "../lang/en_us.json"
+    }
+
+    return langFile;
+}
+
 // Read Json File to translate - TO DO
-fetch('../lang/pt_br.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+// Translate using the correct JSON file
+readTextFile(getLangFile(), function(text){
+    var array = JSON.parse(text);
+
+    for (const [key, value] of Object.entries(array)) {
+        $(`#${key}`).html(value)
+    }
+});
